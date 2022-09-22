30f9a2a46531cb983204a3acf9675650d8c6b243
hakan bahşiş <hakan.bahsis@hotmail.com>
2022-09-23 00:19:21 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/Context/SimpleContextDb.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework.Context
+{
+    public class SimpleContextDb:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-NBOA8Q1\LENOVO;Database=DemoDb;Integrated Security=true");
+
+        }
+        public DbSet<User>? Users { get; set; }
+        public DbSet<OperationClaim>? OperationClaims { get; set; }
+        public DbSet<UserOperationClaim>? UserOperationClaims{ get; set; }
+    }
+}
---
+++ WebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+using System.Data;
+using DataAccess.Concrete.EntityFramework.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+         SimpleContextDb context = new SimpleContextDb();
+        
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            context.Users.ToList();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
